7b97ba8769c9168a86c26358dc4ca7c7d7aa22b8
Jaime Sánchez <jimisanchezm@hotmail.com>
2025-11-18 19:10:38 -0500

    Update Interests.jsx

--- src/Interests.jsx
+++ src/Interests.jsx
@@ -2,7 +2,7 @@ export default function Interests() {
     return (
         <section className="interests-section">
             <h2>Interests</h2>
-            <p>Techonologies and videogames fan, music, animals and nature lover. I enjoy travelling and sharing experiences with my dear ones. Anime is one of the things that I enjoy the most.</p>
+            <p>Techonologies and videogames fan, music, animals and nature lover. Anime is one of the things that I enjoy the most.</p>
         </section>
     )
 }
